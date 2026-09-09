4cfce586b7012fdbcb282aa6c992cfc59f04ebdd
appbuilderlee2 <appbuilderlee2@gmail.com>
2026-09-09 13:50:49 +0930

    Bump Trail Pocket cache to v4.1.1

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const PREFIX =
   "trail-pocket-shell:" + new URL(self.registration.scope).pathname + ":";
-const VERSION = PREFIX + "v4.1.0-r2";
+const VERSION = PREFIX + "v4.1.1";
 const ASSETS = [
   "./unified-ui.mjs",
   "./unified-ui.css",
