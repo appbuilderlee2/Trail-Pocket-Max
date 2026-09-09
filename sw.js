4cc8a769f87a88a3c295227ee9ab342f6957530d
appbuilderlee2 <appbuilderlee2@gmail.com>
2026-09-09 14:22:58 +0930

    Bump PWA shell to v4.1.9

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const PREFIX =
   "trail-pocket-shell:" + new URL(self.registration.scope).pathname + ":";
-const VERSION = PREFIX + "v4.1.8";
+const VERSION = PREFIX + "v4.1.9";
 const ASSETS = [
   "./unified-ui.mjs",
   "./unified-ui.css",
